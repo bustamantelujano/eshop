2a117aee63eff3490739eb809440ac328073faa2
alexis <aleexiis26@gmail.com>
2016-11-09 00:25:15 -0700

    Commit Alexis

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,10 +80,12 @@
                                                      document.getElementById('logout-form').submit();">
                                             Logout
                                         </a>
-
                                         <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
                                             {{ csrf_field() }}
                                         </form>
+
+
+                                        <a href="{{url('/perfil')}} ">Perfil</a>
                                     </li>
                                 </ul>
                             </li>
